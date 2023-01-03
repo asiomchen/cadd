a7c95eba59b23aaf1a91543ae70d26238faf7d96
anton <41703271+asiomchen@users.noreply.github.com>
2023-01-03 22:43:05 +0100

    Centroid refactored

--- utils/centroid.py
+++ utils/centroid.py
@@ -3,7 +3,6 @@ import subprocess
 
 import numpy as np
 
-
 def from_sdf_to_xyz(path, silent: bool = True):
     """
     Convert sdf file to xyz file
@@ -30,7 +29,7 @@ def from_pdbqt_to_xyz(path, silent: bool = True):
     Returns: None
 
     """
-    path_to_xyz = path.replace('.pdbqt', '.xyz')
+    path_to_xyz = os.path.join(os.path.dirname(path), os.path.basename(path).replace('.pdbqt', '.xyz'))
     cmd = "obabel " + path + " -O " + path_to_xyz
     if not silent:
         print(cmd)
@@ -38,11 +37,12 @@ def from_pdbqt_to_xyz(path, silent: bool = True):
     return path_to_xyz
 
 
-def xyz_to_numpy(path):
+def xyz_to_numpy(path, remove_file: bool = True):
     """
     Convert xyz file to numpy array
     Args:
         path: path to xyz file
+        remove_file: bool (default: True)
 
     Returns: None
 
@@ -56,30 +56,70 @@ def xyz_to_numpy(path):
         for i in range(n_mols):
             for j in range(n_atoms):
                 xyz[j] = lines[i * (n_atoms + 2) + 2 + j].split()[1:4]
-            yield xyz
-    os.remove(path)
+            yield xyz.copy()
 
 
 def centroid(xyz_generator):
     """
-    Calculate centroid of a molecule
+    Calculate centroid and coordinates of the molecule
     Args:
         xyz_generator: generator of xyz files
 
     Returns: None
 
     """
+    centroids = []
+    coords = []
     for xyz in xyz_generator:
-        yield np.mean(xyz, axis=0)
+        centroid = np.mean(xyz, axis=0)
+        centroids.append(centroid)
+        coords.append(xyz)
+    return centroids, coords
+
+
+def get_box(xyz, extend=2):
+    """
+    Get box around coordinates
+    Args:
+        xyz: numpy array of xyz coordinates
+        extend: int (default: 2)
+
+    Returns: None
+
+    """
+    min_x = np.min(xyz[:, 0]) - extend
+    max_x = np.max(xyz[:, 0]) + extend
+    min_y = np.min(xyz[:, 1]) - extend
+    max_y = np.max(xyz[:, 1]) + extend
+    min_z = np.min(xyz[:, 2]) - extend
+    max_z = np.max(xyz[:, 2]) + extend
+
+    size_x = max_x - min_x
+    size_y = max_y - min_y
+    size_z = max_z - min_z
+    center_x = (max_x + min_x) / 2
+    center_y = (max_y + min_y) / 2
+    center_z = (max_z + min_z) / 2
+
+    return [center_x, center_y, center_z], [size_x, size_y, size_z]
 
 
 if __name__ == '__main__':
-    from_sdf_to_xyz('1-CHEMBL1077.sdf')
-    print(xyz_to_numpy('1-CHEMBL1077.xyz'))
-    for xyz in centroid(xyz_to_numpy('1-CHEMBL1077.xyz')):
-        print(xyz)
+    os.chdir('../data/test/')
+    file = '1_1_359.pdbqt'
+    conv = from_pdbqt_to_xyz(file)
+    xyz = xyz_to_numpy(conv)
+    res = centroid(xyz)
+    print(res[0][0])
 
-    data = {'name': [],
-            'pose': [],
-            'centroid': []
-            }
+    # center, size = get_box(xyz)
+    # print('Box:', [*center, *size])
+    # all_boxes.append(['gridbox', *center, *size])
+
+    # for xyz in centroid(xyz_to_numpy(file.replace('.pdbqt', '.xyz'))):
+    #     print(xyz)
+    #
+    # data = {'name': [],
+    #         'pose': [],
+    #         'centroid': []
+    #         }
