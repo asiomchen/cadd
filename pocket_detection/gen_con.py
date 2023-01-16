a58922289eef3949e3020b0977d43ed763a6b6de
anton <41703271+asiomchen@users.noreply.github.com>
2023-01-16 17:08:27 +0100

    Mols generator fixes

--- pocket_detection/gen_con.py
+++ pocket_detection/gen_con.py
@@ -6,9 +6,9 @@ import numpy as np
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Draw, SaltRemover
-from utils.convert_alt import to_pdbqt
+from utils.convert_alt import to_pdbqt, to_sdf
 
-df = pd.read_csv('pocket_random_subset.csv', sep=',')
+df = pd.read_csv('/home/anton/PycharmProjects/cadd/pocket_detection/pocket_random_subset.csv', sep=',')
 
 ps_smi = r'OC(C1=CC=C(C=C1)/C2=C3C=CC(/C(C4=C(F)C(F)=C(F)C(F)=C4F)=C5N/C(C=C\5)=C(C6=N/C(C=C6)=C(C7=CC=C2N7)/C8=C(F)C(F)=C(F)C(F)=C8F)/C9=C(F)C(F)=C(F)C(F)=C9F)=N\3)=O'
 ps = Chem.MolFromSmiles(ps_smi)
@@ -18,6 +18,7 @@ link = Chem.MolFromSmiles(link_smi)
 reactor = Reactor()
 conjugates = []
 errors = []
+print(df.head())
 for smiles in df['Smiles']:
     smiles = smiles.split('.')[0].replace('[O-]', 'O')
     mol = Chem.MolFromSmiles(smiles)
@@ -33,17 +34,17 @@ print('Errors: ')
 for smiles in errors:
     print(smiles)
 
-with open('pocket_conjugates.smi', 'w') as f:
+with open('pocket_conjugates_2.smi', 'w') as f:
     f.writelines(conjugates)
 
 ids = df['ID'].tolist()
 ps_id = 1
 link_id = 1
-os.mkdir(f'../notebooks/pocket_set')
-os.chdir(f'../notebooks/pocket_set')
+os.mkdir(f'../notebooks/sdf_pocket_set')
+os.chdir(f'../notebooks/sdf_pocket_set')
 for ids, conjugate in zip(ids, conjugates):
     name = f'{ps_id}_{link_id}_{ids}'
     print(conjugate)
     print(name)
     if conjugate != 'None':
-        to_pdbqt(conjugate, name)
+        to_sdf(conjugate, name)
