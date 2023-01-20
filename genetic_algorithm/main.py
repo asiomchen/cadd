c6f44dcedd91ee329bf655c301aa399fc29f0c1a
anton <41703271+asiomchen@users.noreply.github.com>
2023-01-20 17:07:09 +0100

    Fully working GA with elitism and roulette selection

--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -51,7 +51,7 @@ class Compound:
     Base class for fragment-base compound descriptor
     '''
 
-    def __init__(self, ps, link, lig, name='compound'):
+    def __init__(self, ps, link, lig, name='compound', parent_1=None, parent_2=None, is_mutated=False):
         self.ps = self._sanitize(ps)
         self.link = self._sanitize(link)
         self.lig = self._sanitize(lig)
@@ -62,9 +62,13 @@ class Compound:
         self.name = name
         self.is_generated = os.path.exists('./generated_mols/' + self.name + '.pdbqt')
         self.is_docked = os.path.exists('./docked_mols/' + self.name + '.pdbqt')
+        self.is_mutated = is_mutated
+        self.score = 0
+        self.parent_1 = parent_1
+        self.parent_2 = parent_2
 
     def __str__(self):
-        return f'Compound(name={self.name}, ps={self.ps}, link={self.link}, lig={self.lig})'
+        return f'Compound(name={self.name}, parent_1={self.parent_1.name if self.parent_1 is not None else None}, parent_2={self.parent_2.name if self.parent_2 is not None else None}, is_mutated={self.is_mutated})'
 
     def __repr__(self):
         return f'Compound({self.name})'
@@ -91,10 +95,12 @@ class Compound:
              out_dir='./docked_mols'):
         scores = []
         if self.conjugate is None:
+            self.score = 0
             return [0, 0, 0]
         if self.is_docked:
-            # print('Already docked')
-            return extract_scores(out_dir + '/' + self.name + '.pdbqt')
+            scores = extract_scores(out_dir + '/' + self.name + '.pdbqt')
+            self.score = np.mean(scores)
+            return scores
 
         if self.is_generated:
             ligand = './generated_mols/' + self.name + '.pdbqt'
@@ -112,12 +118,14 @@ class Compound:
                        "--size_x 34.751 --size_y 43.7889 --size_z 34.821"
         out_name = out_dir + '/' + self.name + '.pdbqt'
         cmd = cmd_template.format(protein, ligand, ex, out_name)
-        return_code = subprocess.call(cmd, shell=True)
+        print(f'Entering docking for {self.name}')
+        return_code = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if return_code != 0:
             print('Error in docking, smiles: ', self.conjugate)
-            return None
+            return [0, 0, 0]
         else:
             scores = extract_scores(out_name)
+            print(f'Exiting docking for {self.name}, scores: {scores}')
             return scores
 
     def _sanitize(self, smiles):
@@ -130,29 +138,42 @@ class Compound:
         smiles_compounds = [self.ps, self.link, self.lig]
         mols = list(map(Chem.MolFromSmiles, smiles_compounds))
         reactor = Reactor()
-        print('PS: ', self.ps)
-        print('Link: ', self.link)
-        print('Lig: ', self.lig)
         mol = reactor.conjugate(*mols)
         if mol is None:
             return None
         Chem.SanitizeMol(mol)
         return Chem.MolToSmiles(mol)
 
-    def cross(self, other):
+    def cross(self, other, mutation_rate=0.333):
         '''
         Cross two compounds
         :param other:
         :return:
         '''
         p = [0.5, 0.5]
+        # generate mutation mask
+        mask = np.random.choice([0, 1], size=3, p=[1 - mutation_rate, mutation_rate])
         ps = np.random.choice([self.ps, other.ps], p=p)
+        is_mutated = any(mask)
+        if mask[0] == 1:
+            ps = np.random.choice(pss)
         link = np.random.choice([self.link, other.link], p=p)
+        if mask[1] == 1:
+            link = np.random.choice(links)
         lig = np.random.choice([self.lig, other.lig], p=p)
-        return Compound(ps, link, lig)
+        if mask[2] == 1:
+            lig = np.random.choice(ligs)
+        name = f'{pss.index(ps)}_{links.index(link)}_{ligs.index(lig)}'
+        offspring = Compound(ps, link, lig, name=name, parent_1=self, parent_2=other, is_mutated=is_mutated)
+        if offspring.conjugate is None:
+            print('Error in reaction for compound in .cross(): ', name)
+            print('Scoring will be skipped')
+            offspring.name = 'ERROR_' + name
+        offspring.reset()
+        return offspring
 
     def mutate(self, mutation_rate=0.1):
-        new_compound = Compound(self.ps, self.link, self.lig, name=self.name)
+        new_compound = Compound(self.ps, self.link, self.lig, name=self.name, parent_1=self)
         if random.random() < mutation_rate:
             new_ps = random.choice(pss)
             new_compound.ps = new_ps
@@ -263,14 +284,25 @@ class GeneticDocker:
             self.current_population = self.next_population
             self.next_population = None
         print('Current compound names are: ', [c.name for c in self.current_population])
+        print('{} compounds to dock'.format(sum([not c.is_docked for c in self.current_population])))
         for compound in self.current_population:
+            if not compound.is_docked:
+                compound.reset()
+            # print('Docking compound: ', compound.name)
+            # print('Compound is docked: ', compound.is_docked)
             all_scores[compound] = compound.dock()
+        # apply elitism select only population_size best compounds
+
         self.current_iteration += 1
         mean_scores = {compound: np.mean(scores) for compound, scores in all_scores.items()}
         # sort by mean score
         sorted_scores = sorted(mean_scores.items(), key=lambda x: x[1])
+        print('Applying elitism...')
+        self.current_population = [x[0] for x in sorted_scores[:self.population_size]]
+        sorted_scores = [x for x in sorted_scores[:self.population_size]]
+        print('Current population size: ', len(self.current_population))
         # select 3 mols with probability proportional to their score
-        selected = []
+        parents = []
 
         # caclulate probability of selection normalized to 1
         scores = [x[1] for x in sorted_scores]
@@ -281,25 +313,35 @@ class GeneticDocker:
         # in range of half of population size generate random number and select compound
         # Starting from the top of the population, keep adding the finesses to the partial sum P, till P<S
         # The individual for which P exceeds S is the chosen individual.
-        for i in range(self.population_size // 2):
-            rand = random.random()
-            partial_sum = 0
-            for j in range(len(scores)):
-                partial_sum += scores[j]
-                if partial_sum >= rand:
-                    selected.append(compounds_only[j])
-                    break
+        all_parents = np.random.rand(int(self.population_size))
+        # split parents into 2 groups
+        parents_1 = all_parents[:int(self.population_size / 2)]
+        parents_2 = all_parents[int(self.population_size / 2):]
 
-        # mutate selected compounds
-        pre_mutated = np.random.choice([x[0] for x in sorted_scores], p=scores, size=self.population_size // 2,
-                                       replace=False)
-        mutated = [c.mutate(mutation_rate=0.333) for c in pre_mutated]
-        print(type(mutated))
-        print(type(selected))
-        joined = selected + mutated
-        while len(joined) < self.population_size:
-            joined.append(np.random.choice([x[0] for x in sorted_scores], p=scores))
-            joined = [x for x in joined if x.name.contains('ERROR') is False]
+        # generate offsprings
+        offsprings = []
+        parent_compounds = []
+        for i in range(int(self.population_size / 2)):
+            parent_1 = compounds_only[np.where(np.cumsum(scores) > parents_1[i])[0][0]]
+            parent_2 = compounds_only[np.where(np.cumsum(scores) > parents_2[i])[0][0]]
+            offsprings.append(parent_1.cross(parent_2))
+            parent_compounds.append(parent_1)
+            parent_compounds.append(parent_2)
+        print(f'{len(offsprings)} offsprings generated from {len(all_parents)} parents')
+
+        # in same fashion select subset of population to mutate
+        mutate_rolls = np.random.rand(int(self.population_size / 2))
+        pre_mutated = parents.copy()
+
+        mutated = [c.mutate(mutation_rate=0.2) for c in pre_mutated]
+        # select best 20% of original population
+        elite = sorted_scores[:int(self.population_size / 5)]
+        elite = [x[0] for x in elite]
+        # replace worst half of population with offsprings
+        joined = parent_compounds + offsprings + elite
+        # while len(joined) < self.population_size:
+        #     joined.append(np.random.choice([x[0] for x in sorted_scores], p=scores))
+        #     joined = [x for x in joined if x.name.contains('ERROR') is False]
         self.next_population = joined
         print('Next population size is: ', len(self.next_population))
         self._generate_population(self.next_population)
@@ -307,17 +349,24 @@ class GeneticDocker:
         return sorted_scores
 
 
-# comp = Compound(pss[0], links[0], ligs[0])
-# for i in range(10):
-#     comp = comp.mutate()
-#     print(comp.name)
-# scores = comp.dock(ex=8)
-# print(scores)
+# comp = Compound(pss[0], links[0], ligs[0], name='0_0_0')
+# comp_2 = Compound(pss[5], links[3], ligs[100], name='5_3_100')
+# comp_3 = comp.cross(comp_2)
+# print(comp_3)
+
+def print_linage(compound):
+    if compound.parent_1 is not None:
+        print_linage(compound.parent_1)
+    if compound.parent_2 is not None:
+        print_linage(compound.parent_2)
+    else:
+        print(compound.name)
+        print('-------')
 
 
-GA = GeneticDocker(population_size=10)
+GA = GeneticDocker(population_size=50)
 GA.set_parts(pss, links, ligs)
-for i in range(20):
+for i in range(10):
     scores = GA.run_iteration()
     print(scores)
     print('-----------------------')
@@ -331,4 +380,37 @@ for iter in GA.history:
     sorted_scores = sorted(mean_scores.items(), key=lambda x: x[1])
     mean_scores_list.append(np.mean([x[1] for x in sorted_scores]))
 plt.plot(mean_scores_list)
+plt.title('Mean score per generation')
+plt.xlabel('Generation')
+plt.ylabel('Mean score [kcal/mol]')
 plt.show()
+# plot best score per generation
+best_scores_list = []
+for iter in GA.history:
+    print(iter)
+    # calucate generation mean
+    mean_scores = {compound: np.mean(scores) for compound, scores in GA.history[iter].items()}
+    # sort by generation
+    sorted_scores = sorted(mean_scores.items(), key=lambda x: x[1])
+    best_scores_list.append(sorted_scores[0][1])
+plt.plot(best_scores_list)
+plt.title('Best score per generation')
+plt.xlabel('Generation')
+plt.ylabel('Best score [kcal/mol]')
+plt.show()
+# print best compound linage
+# print_linage(sorted_scores[0][0])
+print('Best compound: ', sorted_scores[0][0].name)
+print('Best compound score: ', sorted_scores[0][1])
+if sorted_scores[0][0].parent_1 is not None and sorted_scores[0][0].parent_2 is not None:
+    print('Best compound parents: ', sorted_scores[0][0].parent_1.name, sorted_scores[0][0].parent_2.name)
+    print('Best compound parents scores: ', sorted_scores[0][0].parent_1.score, sorted_scores[0][0].parent_2.score)
+    if sorted_scores[0][0].parent_1.parent_1 != 'None':
+        print('Best compound grandparents: ', sorted_scores[0][0].parent_1.parent_1.name,
+              sorted_scores[0][0].parent_1.parent_2.name, sorted_scores[0][0].parent_2.parent_1.name,
+              sorted_scores[0][0].parent_2.parent_2.name)
+        print('Best compound grandparents scores: ', sorted_scores[0][0].parent_1.parent_1.score,
+              sorted_scores[0][0].parent_1.parent_2.score, sorted_scores[0][0].parent_2.parent_1.score,
+              sorted_scores[0][0].parent_2.parent_2.score)
+print('Is best compound mutated: ', sorted_scores[0][0].is_mutated)
+print('Is best compound offspring: ', sorted_scores[0][0].conjugate)
