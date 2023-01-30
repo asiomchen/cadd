ae9452ed11772311be23f4ec49b3887afc88b5bd
anton <41703271+asiomchen@users.noreply.github.com>
2023-01-30 11:55:39 +0100

    checkpoint generation

---
+++ genetic_algorithm/other.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import os, sys
+
+sys.path.insert(0, '..')
+from utils.extract_scores import extract_scores
+
+
+def retrive_chkpt(path):
+    chk = {}
+    for file in os.listdir(path):
+        if file.endswith('.pdbqt'):
+            base_name = file.split('.')[0]
+            chk[base_name] = np.mean(extract_scores(path + '/' + file))
+
+    return pd.DataFrame.from_dict(chk, orient='index', columns=['score'])
+
+
+if __name__ == '__main__':
+    path = sys.argv[1]
+    chk = retrive_chkpt(path)
+    print(chk)
